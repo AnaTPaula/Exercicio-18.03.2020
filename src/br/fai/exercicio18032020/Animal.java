676651f74d03a7546c1e1ee864dc3e50a5a2dbf2
Ana Terezinha <aterezinha.19@hotmail.com>
2020-03-25 05:28:28 -0300

    Adicionar corpo do código Animal

--- src/br/fai/exercicio18032020/Animal.java
+++ src/br/fai/exercicio18032020/Animal.java
@@ -5,6 +5,20 @@ public abstract class Animal {
 	int id;
 	String nome;
 	
+	public int getID() {
+		return id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 	
 	
 
